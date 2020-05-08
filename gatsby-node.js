e32c67ec374d043d3c0c4646f3f507351d23335c
Eric McKinney <mckinney99@gmail.com>
2020-05-07 22:32:06 -0500

    added tags for blog posts

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,13 @@
 const path = require(`path`)
+const _ = require("lodash")
+
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
   const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
+  const tagTemplate = path.resolve("src/templates/tags.js")
+
 
   const result = await graphql(`
     {
@@ -17,9 +21,16 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
             frontmatter {
               path
             }
+            frontmatter {
+              tags
+            }
           }
         }
       }
+      tagsGroup: allMarkdownRemark(limit: 2000) {
+        group(field: frontmatter___tags) {
+          fieldValue
+        }
     }
   `)
 
@@ -34,6 +45,27 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       path: node.frontmatter.path,
       component: blogPostTemplate,
       context: {}, // additional data can be passed via context
+    })
+  })
+
+  const posts = result.data.postsRemark.edges
+  // Create post detail pages
+  posts.forEach(({ node }) => {
+    createPage({
+      path: node.fields.slug,
+      component: blogPostTemplate,
+    })
+  })
+  // Extract tag data from query
+  const tags = result.data.tagsGroup.group
+  // Make tag pages
+  tags.forEach(tag => {
+    createPage({
+      path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
+      component: tagTemplate,
+      context: {
+        tag: tag.fieldValue,
+      },
     })
   })
 }
