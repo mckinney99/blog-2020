595c441939bcfbf1c34d8b54b991f51721f65ed6
Eric McKinney <mckinney99@gmail.com>
2020-05-10 17:16:49 -0500

    ignored creating tags for now

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,13 +1,9 @@
 const path = require(`path`)
-const _ = require("lodash")
-
 
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
   const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
-  const tagTemplate = path.resolve("src/templates/tags.js")
-
 
   const result = await graphql(`
     {
@@ -21,16 +17,9 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
             frontmatter {
               path
             }
-            frontmatter {
-              tags
-            }
           }
         }
       }
-      tagsGroup: allMarkdownRemark(limit: 2000) {
-        group(field: frontmatter___tags) {
-          fieldValue
-        }
     }
   `)
 
@@ -45,27 +34,6 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       path: node.frontmatter.path,
       component: blogPostTemplate,
       context: {}, // additional data can be passed via context
-    })
-  })
-
-  const posts = result.data.postsRemark.edges
-  // Create post detail pages
-  posts.forEach(({ node }) => {
-    createPage({
-      path: node.fields.slug,
-      component: blogPostTemplate,
-    })
-  })
-  // Extract tag data from query
-  const tags = result.data.tagsGroup.group
-  // Make tag pages
-  tags.forEach(tag => {
-    createPage({
-      path: `/tags/${_.kebabCase(tag.fieldValue)}/`,
-      component: tagTemplate,
-      context: {
-        tag: tag.fieldValue,
-      },
     })
   })
 }
